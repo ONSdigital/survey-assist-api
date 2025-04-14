6bc944c9e30bafeb9d9696ea6cd205b46293a290
dl-ons <dominic.love@ons.gov.uk>
2025-04-14 11:56:39 +0100

    integrate swagger into fastapi

--- api/main.py
+++ api/main.py
@@ -9,16 +9,23 @@ from api.routes.v1.config import router as config_router
 from api.routes.v1.sic_lookup import router as sic_lookup_router
 
 app: FastAPI = FastAPI(
-    title="LLM API",
-    description="API for interacting with LLM",
-    version="1.0",
+    title="Survey Assist API",
+    description="API for interacting with backend data processing services such as classification",
+    version="1.0.0",
+    contact={
+        "name": "ONS Digital",
+        "email": "steve.gibbard@ons.gov.uk"
+    },
+    openapi_url="/v1/survey-assist/openapi.json",
+    docs_url="/v1/survey-assist/docs",
+    redoc_url="/v1/survey-assist/redoc",
 )
 
 # Include versioned routes
 app.include_router(config_router, prefix="/v1/survey-assist")
 app.include_router(sic_lookup_router, prefix="/v1/survey-assist")
 
-@app.get("/")
+@app.get("/", tags=["Root"])
 def read_root() -> dict[str, str]:
     """Root endpoint for the Survey Assist API.
 
