1c5bde7f22bdee2973fbf645310ec09dd9ff9041
dl-ons <dominic.love@ons.gov.uk>
2025-05-28 15:14:27 +0100

    add result router

--- api/main.py
+++ api/main.py
@@ -9,6 +9,7 @@ from fastapi import FastAPI
 from api.routes.v1.classify import router as classify_router
 from api.routes.v1.config import router as config_router
 from api.routes.v1.embeddings import router as embeddings_router
+from api.routes.v1.result import router as result_router
 from api.routes.v1.sic_lookup import router as sic_lookup_router
 
 app: FastAPI = FastAPI(
@@ -25,6 +26,7 @@ app.include_router(config_router, prefix="/v1/survey-assist")
 app.include_router(embeddings_router, prefix="/v1/survey-assist")
 app.include_router(sic_lookup_router, prefix="/v1/survey-assist")
 app.include_router(classify_router, prefix="/v1/survey-assist")
+app.include_router(result_router, prefix="/v1/survey-assist")
 
 
 @app.get("/", tags=["Root"])
