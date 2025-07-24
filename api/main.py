835a2b4af6fa513e1da2889fc0dc94ab9929c6cf
dlons <dominic.love@ons.gov.uk>
2025-07-24 20:17:45 +0100

    Fix mypy type ignore comment placement

--- api/main.py
+++ api/main.py
@@ -15,8 +15,8 @@ from api.routes.v1.embeddings import router as embeddings_router
 from api.routes.v1.result import router as result_router
 from api.routes.v1.sic_lookup import router as sic_lookup_router
 
-from occupational_classification_utils.llm.llm import (  # isort: skip
-    ClassificationLLM as SOCLLM,  # type: ignore # mypy: disable-error-code="import-not-found"  # pylint: disable=line-too-long
+from occupational_classification_utils.llm.llm import (  # type: ignore # isort: skip
+    ClassificationLLM as SOCLLM,  # mypy: disable-error-code="import-not-found"  # pylint: disable=line-too-long
 )
 
 
